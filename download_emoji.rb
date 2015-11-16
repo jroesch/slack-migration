f69335fbdf5b08b8b73446b8f2d9a400914ccba0
Jared Roesch <roeschinc@gmail.com>
2015-11-16 01:27:26 -0800

    Clean up repository

--- download_emoji.rb
+++ download_emoji.rb
@@ -9,31 +9,20 @@ conn = Faraday.new(url: 'http://api.hipchat.com')
 response = conn.get "/v2/emoticon?auth_token=#{HIPCHAT}"
 json = JSON::parse(response.body)
 
-emoticons = json['items'].map do |item|
-  { name: item["shortcut"], src: item["url"] }
-end
-
-begin
-  # Dir.mkdir('emoticons')
-  # # If the directory exists the exception will be thrown,
-  # # and we won't re-download the emojis
-  # Dir.chdir("emoticons") do
-  #   cwd = Dir.pwd
-  #   emoticons.each do |emoticon|
-  #     file_name = emoticon[:url].split("/").last
-  #     ext = file_name.split(".").last
-  #     image_bytes = Faraday.get(emoticon[:url]).body
-  #     file = open(cwd + '/' + emoticon[:name] + '.' + ext, 'wb')
-  #     file.write(image_bytes)
-  #     file.close
-  #   end
-  # end
-rescue Errno::EEXIST
+emojis = json['items'].map do |item|
+  { "name" => item["shortcut"],
+    "src" => item["url"].gsub('.png', '@4x.png')
+                        .gsub('.jpg', '@4x.jpg')
+                        .gsub('.gif', '@4x.gif')
+  }
 end
 
 emojipack = {
-  title: 'uw-cse'
-  emojis: emoticons.map
+  'title' => 'uw-cse',
+  'emojis' => emojis
 }
 
-binding.pry
+f = open('uw-cse.yaml', 'w')
+f.write(emojipack.to_yaml())
+
+puts "Complete"
