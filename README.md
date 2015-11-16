4ff88f381175a5bf299a394dc404653adeb22104
Jared Roesch <roeschinc@gmail.com>
2015-11-16 01:08:53 -0800

    Sync

---
+++ README.md
@@ -0,0 +1,2 @@
+We are using this tool to upload emojis in bulk:
+https://github.com/lambtron/emojipacks
--- download_emoji.rb
+++ download_emoji.rb
@@ -2,8 +2,38 @@
 require_relative "auth"
 
 require 'pry'
-require 'hipchat'
+require 'faraday'
+require 'json'
 
-client = HipChat::Client.new(HIPCHAT, :api_version => 'v2')
+conn = Faraday.new(url: 'http://api.hipchat.com')
+response = conn.get "/v2/emoticon?auth_token=#{HIPCHAT}"
+json = JSON::parse(response.body)
 
-puts client
+emoticons = json['items'].map do |item|
+  { name: item["shortcut"], src: item["url"] }
+end
+
+begin
+  # Dir.mkdir('emoticons')
+  # # If the directory exists the exception will be thrown,
+  # # and we won't re-download the emojis
+  # Dir.chdir("emoticons") do
+  #   cwd = Dir.pwd
+  #   emoticons.each do |emoticon|
+  #     file_name = emoticon[:url].split("/").last
+  #     ext = file_name.split(".").last
+  #     image_bytes = Faraday.get(emoticon[:url]).body
+  #     file = open(cwd + '/' + emoticon[:name] + '.' + ext, 'wb')
+  #     file.write(image_bytes)
+  #     file.close
+  #   end
+  # end
+rescue Errno::EEXIST
+end
+
+emojipack = {
+  title: 'uw-cse'
+  emojis: emoticons.map
+}
+
+binding.pry
